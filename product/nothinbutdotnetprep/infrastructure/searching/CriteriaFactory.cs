7defd9997b1d9a33502efd1bd643a3ea3cef3cfc
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-01-24 18:28:53 -0500

    Working on negating

--- product/nothinbutdotnetprep/infrastructure/searching/CriteriaFactory.cs
+++ product/nothinbutdotnetprep/infrastructure/searching/CriteriaFactory.cs
@@ -1,9 +1,12 @@
-﻿namespace nothinbutdotnetprep.infrastructure.searching
+﻿using System;
+
+namespace nothinbutdotnetprep.infrastructure.searching
 {
     public interface CriteriaFactory<ItemToSearch, PropertyType>
     {
         Criteria<ItemToSearch> equal_to(PropertyType value);
         Criteria<ItemToSearch> equal_to_any(params PropertyType[] values);
         Criteria<ItemToSearch> not_equal_to_any(params PropertyType[] values);
+        Criteria<ItemToSearch> create_using(Criteria<PropertyType> real_criteria);
     }
 }
